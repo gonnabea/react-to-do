62a9bac2d85d337e4a13aebf38bf5aaa2493ca67
Choi Jiwon <winning1993@naver.com>
2020-06-26 23:52:00 +0900

    Updates

---
+++ precache-manifest.bd9fde1e8adfb6315052d58fe84a309a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e456a4a26df299c39cd6e72a6e5eb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c3a0959c9644248917c",
+    "url": "/static/css/main.2606fe86.chunk.css"
+  },
+  {
+    "revision": "675a30b24b697ca2a0e5",
+    "url": "/static/js/2.87ac713d.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.87ac713d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3a0959c9644248917c",
+    "url": "/static/js/main.6157f100.chunk.js"
+  },
+  {
+    "revision": "dc5fb7f39af77e471839",
+    "url": "/static/js/runtime-main.b729ad1b.js"
+  }
+]);
